5519d971e6e8ce94b032a281068a6cdc6342b8a6
DinoCzar <ryanb_booking@yahoo.com>
2023-04-30 13:39:12 -0700

    Add weather units of measurement to display

--- src/get-weather.js
+++ src/get-weather.js
@@ -9,14 +9,23 @@ function getWeather(city, latitude, longitude, api_key) {
 	async function retreiveWeatherData() {
 		const response = await fetch(apiStringWeather, { mode: 'cors' });
 		const weatherArray = await response.json();
-        const main = weatherArray.weather[0].main;
+		const main = weatherArray.weather[0].main;
 		const description = weatherArray.weather[0].description;
-		const temp = weatherArray.main.temp;
-		const tempMin = weatherArray.main.temp_min;
-		const tempMax = weatherArray.main.temp_max;
-		const humidity = weatherArray.main.humidity;
-		const windSpeed = weatherArray.wind.speed;
-		displayWeather(city, main, description, temp, tempMin, tempMax, humidity, windSpeed);
+		const temp = Math.floor(weatherArray.main.temp);
+		const tempMin = Math.floor(weatherArray.main.temp_min);
+		const tempMax = Math.floor(weatherArray.main.temp_max);
+		const humidity = Math.floor(weatherArray.main.humidity);
+		const windSpeed = Math.floor(weatherArray.wind.speed);
+		displayWeather(
+			city,
+			main,
+			description,
+			temp,
+			tempMin,
+			tempMax,
+			humidity,
+			windSpeed
+		);
 	}
 	retreiveWeatherData().catch(function (err) {
 		alert('An error occurred while fetching the data.');
@@ -35,24 +44,24 @@ const displayWindSpeed = document.querySelector('#wind-speed');
 
 function displayWeather(
 	city,
-    main,
-    description,
+	main,
+	description,
 	temp,
 	tempMin,
 	tempMax,
 	humidity,
 	windSpeed
 ) {
-    displayCity.textContent = city;
-    displayMain.textContent = main;
+	displayCity.textContent = city;
+	displayMain.textContent = main;
 	displayDescription.textContent = description;
-	displayTemp.textContent = 'Temperature: ' + temp;
-	displayTempMin.textContent = 'Low Temp: ' + tempMin;
-	displayTempMax.textContent = 'High Temp: ' + tempMax;
-	displayHumidity.textContent = 'Humidity: ' + humidity;
-	displayWindSpeed.textContent = 'Wind Speed: ' + windSpeed;
-    const cssSelector = main.charAt(0).toLowerCase() + main.slice(1);
-    displayDiv.classList.add(cssSelector);
+	displayTemp.textContent = 'Temperature: ' + temp + '\u00B0F';
+	displayTempMin.textContent = 'Low Temp: ' + tempMin + '\u00B0F';
+	displayTempMax.textContent = 'High Temp: ' + tempMax + '\u00B0F';
+	displayHumidity.textContent = 'Humidity: ' + humidity + '%';
+	displayWindSpeed.textContent = 'Wind Speed: ' + windSpeed + 'mph';
+	const cssSelector = main.charAt(0).toLowerCase() + main.slice(1);
+	displayDiv.classList.add(cssSelector);
 }
 
 export default getWeather;
